73a80d0ececac69458f821657bbf39e9cc3a61dc
joshuayeo12 <joshuajkyeo@gmail.com>
2024-12-03 13:59:41 +0800

    first

---
+++ src/main/java/ssf/day19inclass/Day19inclassApplication.java
@@ -0,0 +1,13 @@
+package ssf.day19inclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day19inclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day19inclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/day19inclass/Day19inclassApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.day19inclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day19inclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
